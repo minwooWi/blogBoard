d93f1deb1a00f83ff0f995e7fffc81503783cd74
Minwoo Chase <c5287@nate.com>
2022-11-25 17:19:39 +0900

    get post list

--- src/main/java/com/gamin/blogboard/controller/ViewController.java
+++ src/main/java/com/gamin/blogboard/controller/ViewController.java
@@ -1,28 +1,31 @@
 package com.gamin.blogboard.controller;
 
+import com.gamin.blogboard.services.PostsService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class IndexController {
-//
-//    @RequestMapping("/")
-//    public String main(){
-//
-//        return "index";
-//    }
+public class ViewController {
+
+    @GetMapping("/")
+    public String postList() {
+        return "index";
+    }
+
+    @RequestMapping("/post")
+    public String post(){
+
+        return "post";
+    }
 //
 //    @RequestMapping("/about")
 //    public String about(){
 //
 //        return "about";
 //    }
-//
-//    @RequestMapping("/post")
-//    public String post(){
-//
-//        return "post";
-//    }
+
 //
 //    @RequestMapping("/contact")
 //    public String contact(){
