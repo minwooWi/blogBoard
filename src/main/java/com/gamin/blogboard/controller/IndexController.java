c7656b37db2c2a195ca031ccfb7649735ebc510b
Minwoo Chase <c5287@nate.com>
2022-11-25 15:52:10 +0900

    add swagger

--- src/main/java/com/gamin/blogboard/controller/IndexController.java
+++ src/main/java/com/gamin/blogboard/controller/IndexController.java
@@ -5,28 +5,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
-
-    @RequestMapping("/")
-    public String main(){
-
-        return "index";
-    }
-
-    @RequestMapping("/about")
-    public String about(){
-
-        return "about";
-    }
-
-    @RequestMapping("/post")
-    public String post(){
-
-        return "post";
-    }
-
-    @RequestMapping("/contact")
-    public String contact(){
-
-        return "contact";
-    }
+//
+//    @RequestMapping("/")
+//    public String main(){
+//
+//        return "index";
+//    }
+//
+//    @RequestMapping("/about")
+//    public String about(){
+//
+//        return "about";
+//    }
+//
+//    @RequestMapping("/post")
+//    public String post(){
+//
+//        return "post";
+//    }
+//
+//    @RequestMapping("/contact")
+//    public String contact(){
+//
+//        return "contact";
+//    }
 }
